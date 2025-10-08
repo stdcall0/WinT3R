ceb19b0e3c2eda9ab328f32fc1179826abca7ec2
__stdcall <hanghang0702@hotmail.com>
2025-10-08 15:00:00 +0800

    fix huggingface url

--- configure.sh
+++ configure.sh
@@ -2,4 +2,4 @@
 
 pip install -r requirements.txt
 mkdir -p checkpoints
-wget -O checkpoints/pytorch_model.bin https://hf-mirror.com/lizizun/WinT3R/resolve/main/pytorch_model.bin
+wget -O checkpoints/pytorch_model.bin https://huggingface.co/lizizun/WinT3R/resolve/main/pytorch_model.bin
